9e0b6ababe08d41efc89c932d6f458e07147e80b
Manish <manish2003sharma@gmail.com>
2025-06-15 11:37:20 +0530

    Landing Page

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let currentUser = JSON.parse(localStorage.getItem('loggedInUser'));
 let login_btn = document.querySelector('#login_btn');
 let register_btn = document.querySelector('#register_btn');
-// window.location.href = "/Projects/SCJS/login/login.html";
+// window.location.href = "./login/login.html";
 
 
 
 if(currentUser){
-    window.location.href = "/Projects/SCJS/shop/index.html";
+    window.location.href = "./shop/index.html";
 } else {
     login_btn.addEventListener('click', () => {
-    window.location.href = "/Projects/SCJS/login/login.html";
+    window.location.href = "../login/login.html";
 });
 
 register_btn.addEventListener('click', () => {
-    window.location.href = "/Projects/SCJS/login/signup.html";
+    window.location.href = "../login/signup.html";
 });
 }
